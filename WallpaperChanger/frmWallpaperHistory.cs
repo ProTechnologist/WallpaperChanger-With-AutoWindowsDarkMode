1a9157e73f2ef7a3c7a76b16ccfd1323e8fe8709
Umer <pro.technologist@gmail.com>
2024-03-12 00:09:04 +0500

    Responsive UI for loading wallpaper thumbnails.

--- WallpaperChanger/frmWallpaperHistory.cs
+++ WallpaperChanger/frmWallpaperHistory.cs
@@ -52,31 +52,10 @@ namespace WallpaperChanger
                 {
                     PictureBox picture_box = new PictureBox();
                     picture_box.ImageLocation = $"https://th.wallhaven.cc/small/{id.Substring(0, 2)}/{id}.jpg";
-                    //picture_box.Width = 300;
-                    //picture_box.Height = 200;
                     picture_box.SizeMode = PictureBoxSizeMode.AutoSize;
-                    picture_box.Tag = $"https://wallhaven.cc/w/{id}";
+                    picture_box.Tag = id; // $"https://wallhaven.cc/w/{id}";
 
-                    picture_box.DoubleClick += (sender, e) =>
-                    {
-                        string url = (sender as PictureBox).Tag.ToString();
-                        string id = Path.GetFileName(url);
-
-                        #region open wallpaper in the browse (removing this feature in favor of setting image as wallpaper)
-                        //Process.Start(new ProcessStartInfo()
-                        //{
-                        //    UseShellExecute = true,
-                        //    FileName = url
-                        //});
-                        #endregion
-
-                        // set image as wallpaper
-                        new WallhavenAutoWallpaperChanger().ApplyWallpaper(new WallpaperInfo()
-                        {
-                            URL = url,
-                            Path = @$"https://w.wallhaven.cc/full/{ id.Substring(0,2) }/wallhaven-{id}.jpg"
-                        });
-                    };
+                    picture_box.DoubleClick += Picture_box_DoubleClick;
 
                     boxes.Add(picture_box);
                 }
@@ -88,6 +67,26 @@ namespace WallpaperChanger
             // loading up wallpaper boxes into othe UI
             flowLayoutPanel1.Controls.AddRange(boxes.ToArray());
             groupBox1.Text = $"Previous Wallpapers ({boxes.Count})";
+        }
+
+        private void Picture_box_DoubleClick(object? sender, EventArgs e)
+        {
+            string id = (sender as PictureBox).Tag.ToString();
+
+            #region open wallpaper in the browse (removing this feature in favor of setting image as wallpaper)
+            //Process.Start(new ProcessStartInfo()
+            //{
+            //    UseShellExecute = true,
+            //    FileName = url
+            //});
+            #endregion
+
+            // set image as wallpaper
+            new WallhavenAutoWallpaperChanger().ApplyWallpaper(new WallpaperInfo()
+            {
+                URL = $"https://wallhaven.cc/w/{id}",
+                Path = @$"https://w.wallhaven.cc/full/{id.Substring(0, 2)}/wallhaven-{id}.jpg"
+            });
         }
     }
 }
